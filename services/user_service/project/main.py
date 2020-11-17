3393399395d7f3098b62dd2313d3a0a6917b7319
rjh <rory.hedderman@publicissapient.com>
2020-11-17 11:09:02 +0000

    added stock into models

--- services/user_service/project/main.py
+++ services/user_service/project/main.py
@@ -10,30 +10,38 @@ app.config.from_object("project.config.Config")
 
 from project.models import *
 
-parser = reqparse.RequestParser()
-parser.add_argument('x', type=int)
-parser.add_argument('y', type=int)
-
-names= {"mop":{"age":25, "gender": "male"},
-       "bill": {"age":70, "gender":"male"}}
-
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name] 
-    
-class ScriptTest(Resource):
-    def post(self):
-        json_data = request.get_json(force=True)
-        new_data = test_script(json_data['x'], json_data['y'])
-        return {'new_x':new_data[0], 'new_y': new_data[1]} 
+#parser = reqparse.RequestParser()
+#parser.add_argument('x', type=int)
+#parser.add_argument('y', type=int)
        
 class MicroTest(Resource):
     def get(self):
         return {"Message": "User"}
 
+class CheckUser(Resource):
+    def get(self, uname):
+        q_user = User.query.filter_by(username=uname).first()
+
+        if q_user:
+            return {"Exists":1}
+
+        return {"Exists":0}
+
+class AddUser(Resource):
+    def post(self):
+        json_data = request.get_json(force=True)
+        new_user = User(username=json_data['username'])
+        db.session.add(new_user)
+        db.session.commit()
+
+
+
+        return {"Success":1}
+        
+
 api.add_resource(MicroTest, "/" )
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
-api.add_resource(ScriptTest, "/scripttest")
+api.add_resource(CheckUser, "/checkuser/<string:uname>/" )
+api.add_resource(AddUser, "/adduser/" )
 
 if __name__ == "__main__":
     app.run(port = 5004, debug=True)
