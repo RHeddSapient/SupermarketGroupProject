f9bfe059f77d152e09ac691d77826d5bb1168c5e
Ubuntu <ubuntu@ip-172-31-30-138.ec2.internal>
2020-11-17 15:31:33 +0000

    add in shopping list api calls

--- services/shopping_list_service/project/main.py
+++ services/shopping_list_service/project/main.py
@@ -10,30 +10,97 @@ app.config.from_object("project.config.Config")
 
 from project.models import *
 
-parser = reqparse.RequestParser()
-parser.add_argument('x', type=int)
-parser.add_argument('y', type=int)
-
-names= {"mop":{"age":25, "gender": "male"},
-       "bill": {"age":70, "gender":"male"}}
-
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name] 
-    
-class ScriptTest(Resource):
-    def post(self):
-        json_data = request.get_json(force=True)
-        new_data = test_script(json_data['x'], json_data['y'])
-        return {'new_x':new_data[0], 'new_y': new_data[1]} 
-       
 class MicroTest(Resource):
     def get(self):
         return {"Message": "Shop List"}
 
-api.add_resource(MicroTest, "/" )
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
-api.add_resource(ScriptTest, "/scripttest")
+class UserList(Resource):
+    def get(self, u_id):
+        lists = List.query.filter(user_id = u_id)
+        output_list = []
+
+        for list in lists:
+            contents = [ {"id": list.id, "title":list.title, "products": [{"id": prod.id, 
+                         "name":prod.prod_name, "category":prod.prod_category, "stocked":prod.stocked} 
+                         for prod in ListProduct.query.filter(list_id= list.id)] } ]
+            output_list.append(contents)
+        return jsonify(output_list)
+
+    def post(self, u_id):
+        json_data = request.get_json(force=True)
+
+        new_list = List(user_id=u_id, title=json_data['title'])
+        db.session.add(new_list)
+        db.session.commit()
+
+        return {"Success":1, "id": new_list.id}
+
+class ChangeList(Resource):
+    ## add product
+    def post(self, u_id, l_id):
+        json_data = request.get_json(force=True)
+        new_list_product = ListProduct(product_id = json_data['product_id'], list_id = l_id)
+
+        db.session.add(new_list_product)
+        db.session.commit()
+
+        return {"Success":1}
+
+    ## delete list
+    def delete(self, u_id, l_id):
+        ListProduct.query.filter(list_id=l_id).delete()
+        List.query.filter(id=l_id).delete()
+
+        db.session.commit()
+
+        return {"Success":1}
+
+    ## update list name
+    def put(self, u_id, l_id):
+        json_data = request.get_json(force=True)
+
+        list = List.query.filter(id=l_id)
+        list.title = json_data['title']
+
+        db.session.commit()
+
+        return {"Success":1}
+
+    ## return list
+    def get(self, u_id, l_id):
+            list = List.query.filter(id=l_id).first()
+            contents = [
+                        {"id": list.id, "title":list.title, "products": 
+                            [{"id": prod.id, "name":prod.prod_name, "category":prod.prod_category, "stocked":prod.stocked} for prod
+                                in ListProduct.query.filter(list_id= list.id)]
+                        }
+                       ]
+            return jsonify(contents)
+
+
+class DeleteFromList(Resource):
+    def delete(self, u_id, l_id, p_id):
+        ListProduct.query.filter(product_id=p_id, list_id=l_id).delete()
+        db.session.commit()
+
+        return {"Success":1}
+
+class GetAllProducts(Resource):
+    def get(self):
+        st = Product.query.all()
+        all_products = [{"id": prod.id, "name":prod.prod_name, "category":prod.prod_category, 
+                        "stocked":prod.stocked} for product in st]
+
+        return jsonify(all_products)
+
+        
+
+        
+api.add_resource(MicroTest, "/test/" )
+api.add_resource(UserList, "/<int:u_id>/")
+api.add_resource(ChangeList, "/<int:u_id>/<int:l_id>/")
+api.add_resource(DeleteFromList, "/<int:u_id>/<int:l_id>/<int:p_id>/")
+api.add_resource(GetAllProducts, "/allproducts/")
 
 if __name__ == "__main__":
     app.run(port = 5001, debug=True)
