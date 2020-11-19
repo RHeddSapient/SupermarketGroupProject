3600b50cded5095ad611ccfe6fe875e620ae8eb0
root <rory.hedderman@publicissapient.com>
2020-11-19 13:05:01 +0000

    agent config is now built in the route calculation service

--- services/route_calc_service/project/main.py
+++ services/route_calc_service/project/main.py
@@ -40,12 +40,49 @@ class DistributeProducts(Resource):
             
         return {"Success":1}
 
+class GetRoute(Resource):
+    def get(self, s_id, u_id, l_id):
+         st = Store.query.get(s_id)
+#        map_config = {}
+         prod_loc = st.store_map['prod_loc']
+#        map_config['dimensions'] = st.store_map['dimensions']
+#        map_config['obstacles'] = st.store_map['obstacles']
+#        print(prod_loc)
+#        print(map_config)
+         list = List.query.get(l_id)
+         list_prod = ListProduct.query.filter_by(list_id = list.id).all()
+         locs = []
+         for lp in list_prod:
+             locs.append(prod_loc[lp.product_id-1][1])
+         position = Locations.query.filter_by(store_id = s_id, user_id = u_id).first()
+
+         start_pos = []
+         if not position:
+            start_pos = [0,0]
+         else:
+            start_pos = [position.x_loc, position.y_loc]
+
+         username = User.query.get(u_id).username
+            
+         
+
+         agent_config = {"username":username, "start_position": start_pos, "item_coords":locs}
+
+
+
+         return agent_config
+
+
+        
+
+        
         
         
         
 
 api.add_resource(MicroTest, "/test/")
 api.add_resource(DistributeProducts, "/<int:s_id>/setproducts/")
+api.add_resource(GetRoute, "/<int:s_id>/<int:u_id>/<int:l_id>/route/")
 
 if __name__ == "__main__":
     app.run(port=5002,debug=True)
